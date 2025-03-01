10448501d7ffa1029967a55f735ff49ad5fe80b8
Alexandros Polymeros <alpolymer@gmail.com>
2025-03-01 19:06:17 +0200

    added process.env

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -83,6 +83,6 @@ app.put("/api/notes/:id", (req, res) => {
   res.json(updatedNote)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server is  running on port ${PORT}`)
