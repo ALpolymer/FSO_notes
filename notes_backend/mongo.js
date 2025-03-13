6b1fcec91c57aa09deae043bc9a46dd265900db9
Alexandros Polymeros <alpolymer@gmail.com>
2025-03-13 16:44:52 +0200

    find all notes from db

--- notes_backend/mongo.js
+++ notes_backend/mongo.js
@@ -20,12 +20,18 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema)
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
-})
-
-note.save().then((result) => {
-  console.log("note saved!")
+Note.find({}).then((res) => {
+  console.log(res)
   mongoose.connection.close()
 })
+
+// const note = new Note({
+//   content: "Mongoose makes things easy",
+//   important: true,
+// })
+
+// //Save note to db
+// note.save().then((result) => {
+//   console.log("note saved!")
+//   mongoose.connection.close()
+// })
