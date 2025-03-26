b283d4f590bb0faf19c3f5ef930c9a1a4b14e61a
Alexandros Polymeros <alpolymer@gmail.com>
2025-03-26 12:16:36 +0200

    typo

--- notes_backend/models/note.js
+++ notes_backend/models/note.js
@@ -7,28 +7,28 @@ console.log("connecting to", url)
 
 console.log(url)
 
-// mongoose
-//   .connect(url)
-//   .then((result) => {
-//     console.log("connected to MongoB")
-//   })
-//   .catch((error) => {
-//     console.log("error connecting to MongoDB:", error.message)
-//   })
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoB")
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message)
+  })
 
-// const noteSchema = new mongoose.Schema({
-//   content: String,
-//   important: Boolean,
-// })
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
 
-// noteSchema.set("toJSON", {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   },
-// })
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
-// const Note = mongoose.model("Note", noteSchema)
+const Note = mongoose.model("Note", noteSchema)
 
-// module.exports = mongoose.model("Note", noteSchema)
+module.exports = mongoose.model("Note", noteSchema)
